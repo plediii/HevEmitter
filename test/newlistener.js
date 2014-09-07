11c7cfa851148c34a21c13cce1fc06c7fe360d19
Paul Ledbetter <ledbetter.paul+github@gmail.com>
2014-09-07 21:12:10 +0000

    Add constraints for newListener event in once events.

--- test/newlistener.js
+++ test/newlistener.js
@@ -7,41 +7,83 @@ var assert = require('assert');
 
 describe('HevEmitter newlistener', function () {
 
-    it('should be triggered when a listener is added to a "name" event', function (done) {
-        var h = new H();
-        h.on(['newListener'], function () {
-            done();
+
+    describe('on events', function () {
+        it('should be triggered when a listener is added to a "name" event', function (done) {
+            var h = new H();
+            h.on(['newListener'], function () {
+                done();
+            });
+            h.on(['star'], function () {});
         });
-        h.on(['star'], function () {});
+
+        it('should be triggered when a listener is added to a "name" event, with data', function (done) {
+            var h = new H();
+            h.on(['newListener'], function (data) {
+                assert(data);
+                done();
+            });
+            h.on(['star'], function () {});
+        });
+
+
+        it('should be triggered when a listener is added to a "name" event, with data.event equal to the listened to event', function (done) {
+            var h = new H();
+            h.on(['newListener'], function (data) {
+                assert.deepEqual(data.event, ['star']);
+                done();
+            });
+            h.on(['star'], function () {});
+        });
+
+        it('should be triggered when a listener is added to a "name" event, with data.listener equal to the listener function', function (done) {
+            var h = new H();
+            var listener = function () {};
+            h.on(['newListener'], function (data) {
+                assert.equal(data.listener, listener);
+                done();
+            });
+            h.on(['star'], listener);
+        });
     });
 
-    it('should be triggered when a listener is added to a "name" event, with data', function (done) {
-        var h = new H();
-        h.on(['newListener'], function (data) {
-            assert(data);
-            done();
+    describe('once events', function () {
+        it('should be triggered when a listener is added to a "name" event', function (done) {
+            var h = new H();
+            h.on(['newListener'], function () {
+                done();
+            });
+            h.once(['star'], function () {});
         });
-        h.on(['star'], function () {});
-    });
+
+        it('should be triggered when a listener is added to a "name" event, with data', function (done) {
+            var h = new H();
+            h.on(['newListener'], function (data) {
+                assert(data);
+                done();
+            });
+            h.once(['star'], function () {});
+        });
 
 
-    it('should be triggered when a listener is added to a "name" event, with data.event equal to the listened to event', function (done) {
-        var h = new H();
-        h.on(['newListener'], function (data) {
-            assert.deepEqual(data.event, ['star']);
-            done();
+        it('should be triggered when a listener is added to a "name" event, with data.event equal to the listened to event', function (done) {
+            var h = new H();
+            h.on(['newListener'], function (data) {
+                assert.deepEqual(data.event, ['star']);
+                done();
+            });
+            h.once(['star'], function () {});
         });
-        h.on(['star'], function () {});
-    });
 
-    it('should be triggered when a listener is added to a "name" event, with data.listener equal to the listener function', function (done) {
-        var h = new H();
-        var listener = function () {};
-        h.on(['newListener'], function (data) {
-            assert.equal(data.listener, listener);
-            done();
+        it('should be triggered when a listener is added to a "name" event, with data.listener equal to the listener function', function (done) {
+            var h = new H();
+            var listener = function () {};
+            h.on(['newListener'], function (data) {
+                assert.equal(data.listener, listener);
+                done();
+            });
+            h.once(['star'], listener);
         });
-        h.on(['star'], listener);
     });
 
 });
