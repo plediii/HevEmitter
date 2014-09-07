936194cc9519afa66b3176e83b46a69748c33234
Paul Ledbetter <ledbetter.paul+github@gmail.com>
2014-09-07 18:15:38 +0000

    Flatten example.
    With the synchronous constraints, the example doesn't need to use the promise structure.

--- example.js
+++ example.js
@@ -17,42 +17,35 @@ h.on(['finn', 'the', 'peasant'], function (msg) {
 
 console.log('###Emitting a hierarchical event');
 console.log("// waiting on ['finn', 'the', '*'] emission");
-h.emit(['finn', 'the', '*'], 'cake')
-.then(function () {
+h.emit(['finn', 'the', '*'], 'cake');
 
 console.log('###Emitting to all listeners in a hierarchy');
 console.log("// waiting on ['finn', '**'] emission");
-h.emit(['finn', '**'], 'cake')
-    .then(function () {
+h.emit(['finn', '**'], 'cake');
 
-        console.log('###Listening to all events in a hierarchy');
-        h.on(['finn', '**'], function () {
-            console.log('princess bubblegum');
-        });
-        console.log("// waiting on ['finn', 'the', 'human'] emission");
-        h.emit(['finn', 'the', 'human'], 'cake')
-            .then(function () {
-                console.log('###Removing events in a hierarchy');
-                h.on(['*', 'the', '*'], function (msg) {
-                    console.log('Hello ', msg);
-                });
-                h.removeAllListeners(['finn', '**']);
-                console.log("// waiting on ['finn', 'the', '*'] emission");
-                h.emit(['finn', 'the', '*'], 'EventEmitter')
-                    .then(function () {
-                        console.log('###Short circuiting');
-                        h.on(['**'], function (msg, next) {
-                            if (msg === 'picard') {
-                                return next();
-                            }
-                        });
-
-                        console.log("// waiting on ['data', 'the', 'android'] and ['geordi', 'the', 'engineer'] emissions");
-                        h.emit(['data', 'the', 'android'], 'picard');
-                        h.emit(['geordi', 'the', 'engineer'], 'riker');
-                    });
-            });                             
-    });
-
+console.log('###Listening to all events in a hierarchy');
+h.on(['finn', '**'], function () {
+    console.log('princess bubblegum');
 });
+console.log("// waiting on ['finn', 'the', 'human'] emission");
+h.emit(['finn', 'the', 'human'], 'cake');
+
+console.log('###Removing events in a hierarchy');
+h.on(['*', 'the', '*'], function (msg) {
+    console.log('Hello ', msg);
+});
+h.removeAllListeners(['finn', '**']);
+console.log("// waiting on ['finn', 'the', '*'] emission");
+h.emit(['finn', 'the', '*'], 'EventEmitter');
+
+console.log('###Short circuiting');
+h.on(['**'], function (msg, next) {
+    if (msg === 'picard') {
+        return next();
+    }
+});
+
+console.log("// waiting on ['data', 'the', 'android'] and ['geordi', 'the', 'engineer'] emissions");
+h.emit(['data', 'the', 'android'], 'picard');
+h.emit(['geordi', 'the', 'engineer'], 'riker');
 
