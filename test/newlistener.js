a5d677a8e205ccfbe937b15b6123f8e869a542a2
Paul Ledbetter <ledbetter.paul+github@gmail.com>
2015-06-14 02:39:01 +0000

    Refactor newListener tests.

--- test/newlistener.js
+++ test/newlistener.js
@@ -5,6 +5,8 @@ var H = require('../index').EventEmitter;
 var _ = require('lodash');
 var assert = require('assert');
 
+var routes = require('./routes');
+
 describe('HevEmitter newlistener', function () {
 
     var h;
@@ -14,118 +16,89 @@ describe('HevEmitter newlistener', function () {
 
     describe('on events', function () {
 
-        it('should be triggered when a listener is added to a "name" event', function () {
-            var called = 0;
-            h.on(['newListener'], function () {
-                called++;
+        var shouldReceive = function (onRoute, emitRoute) {
+            it('should receive at ' + '"' + onRoute.join('/') + '" when listeners are added at "' + emitRoute.join('/') + '"', function () {
+                var count = 0;
+                h.on(onRoute, function (msg) {
+                    count++;
+                });
+                assert.equal(0, count);
+                h.on(emitRoute, function () {});
+                assert.equal(1, count);
             });
-            assert.equal(0, called, 'bad initial state');
-            h.on(['star'], function () {});
-            assert.equal(1, called, 'newListener was not called');
+
+            it('should receive listener route at ' + '"' + onRoute.join('/') + '" when listeners are added at "' + emitRoute.join('/') + '"', function (done) {
+                h.on(onRoute, function (route) {
+                    assert.deepEqual(route, emitRoute);
+                    done();
+                });
+                h.on(emitRoute, function () {});
+            });
+
+            it('should receive listener function at ' + '"' + onRoute.join('/') + '" when listeners are added at "' + emitRoute.join('/') + '"', function (done) {
+                var f = function () {};
+                h.on(onRoute, function (route, g) {
+                    assert.equal(f, g);
+                    done();
+                });
+                h.on(emitRoute, f);
+            });
+        };  
+
+        var shouldNotReceive = function (onRoute, emitRoute) {
+            it('should *not* receive at ' + '"' + onRoute.join('/') + '" when listeners are added at "' + emitRoute.join('/') + '"', function () {
+                var count = 0;
+                h.on(onRoute, function (msg) {
+                    count++;
+                });
+                assert.equal(0, count);
+                h.on(emitRoute, function () {});
+                assert.equal(0, count);
+            });
+        };
+
+        var shouldBeDeleted = function (onRoute, emitRoute) {
+            it('should not receive at ' + '"' + onRoute.join('/') + '" when listeners are removed at "' + emitRoute.join('/') + '"', function () {
+                var count = 0;
+                var f = function (msg) {
+                    count++;
+                };
+                h.on(onRoute, f);
+                assert.equal(0, count);
+                h.on(onRoute, function () {});
+                assert.equal(1, count, 'listener was not called to begin with');
+                h.removeListener(emitRoute, f);
+                assert.equal(1, count, 'listener was called again after removal');
+            });
+
+            it('should not receive at ' + '"' + onRoute.join('/') + '" when listeners are removed at "' + emitRoute.join('/') + '"', function () {
+                var count = 0;
+                h.on(onRoute, function (msg) {
+                    count++;
+                });
+                assert.equal(0, count);
+                h.on(onRoute, function () {});
+                assert.equal(1, count, 'listener was not called to begin with');
+                h.removeAllListeners(emitRoute);
+                assert.equal(1, count, 'listener was called again after removal');
+            });
+        };
+
+        _.each(_.map(routes.matchRoutes, function (basicMatch) {
+            return [['newListener'], basicMatch[1]];
+        }).concat(_.map(routes.matchRoutes, function (basicMatch) {
+            return [['newListener'].concat(basicMatch[0]), basicMatch[1]];
+        })), function (args) {
+            shouldReceive.apply(null, args);
         });
 
-        it('should *not* be triggered after removing specific listener', function () {
-            var called = 0;
-            var f = function () {
-                called++;
-            }
-            h.on(['newListener'], f);
-            assert.equal(0, called, 'bad initial state');
-            h.removeListener(['newListener'], f);
-            assert.equal(0, called, 'bad initial state');
-            h.on(['star'], function () {});
-            assert.equal(0, called, 'newListener was called after removal');
+        _.each(_.map(routes.matchRoutes, function (basicMatch) {
+            return [['newListener'].concat(basicMatch[0]), basicMatch[1]];
+        }), function (args) {
+            var listenRoute = args[0];
+            shouldNotReceive.apply(null, args);
         });
 
-        it('should *not* be triggered after removing all listeners', function () {
-            var called = 0;
-            var f = function () {
-                called++;
-            }
-            h.on(['newListener'], f);
-            assert.equal(0, called, 'bad initial state');
-            h.removeAllListeners(['newListener'], f);
-            assert.equal(0, called, 'bad initial state');
-            h.on(['star'], function () {});
-            assert.equal(0, called, 'newListener was called after removal');
-        });
-
-
-        it('should be triggered before listener is available', function () {
-            var called = 0;
-            var route = ['four'];
-            assert.equal(0, h.listeners(route), 'initially found listener');
-            var f = function () {
-                assert.equal(0, h.listeners(route).length, 'found  listener prematurely');
-                called++;
-            };
-            h.on(['newListener'], f);
-            h.on(route, function () {});
-            assert.equal(1, called, 'newListener event was not called');
-            assert.equal(1, h.listeners(route).length, 'did not find listener after adding');
-        });
-
-
-        it('should *NOT* trigger * listeners', function (done) {
-            h.on(['*', '*'], function () {
-                done(true);
-            });
-            h.on(['newListener'], function () {
-                done();
-            });
-            h.on(['star'], function () {});
-        });
-
-        it('should *NOT* trigger ** listeners', function (done) {
-            h.on(['**'], function () {
-                done(true);
-            });
-            h.on(['newListener'], function () {
-                done();
-            });
-            h.on(['star'], function () {});
-        });
-
-        it('should be triggered when a listener is added to a "name" event, with data', function (done) {
-            h.on(['newListener'], function (data) {
-                assert(data);
-                done();
-            });
-            h.on(['star'], function () {});
-        });
-
-        it('should be triggered with listener route as first argument', function (done) {
-            var route = ['five', 'ten'];
-            h.on(['newListener'], function (data) {
-                assert.deepEqual(route, data);
-                done();
-            });
-            h.on(route, function () {});
-        });
-
-        it('should be triggered with listener function as second argument', function (done) {
-            var route = ['five', 'ten'];
-            var f = function () {};
-            h.on(['newListener'], function (data, cb) {
-                assert.equal(cb, f, 'did not receive callback function as second argument');
-                done();
-            });
-            h.on(route, f);
-        });
-
-    });
-
-    describe('on sub events', function () {
-        it('should be triggered when a listener is added to a "newlistener/route" event', function (done) {
-            var h = new H();
-            h.on(['newListener', 'powersurge'], function () {
-                done(true);
-            });
-            h.on(['newListener', 'alright'], function () {
-                done();
-            });
-            h.on(['alright'], function () {});
-        });
     });
 
 });
